f58bc95bd8510b9b4cb5231d4b4df092c053e13e
LaviIonas <laviionas772@gmail.com>
2019-02-14 19:43:20 +0000

    aalalala

--- express_server.js
+++ express_server.js
@@ -1,27 +1,45 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
-
 app.get("/", (req, res) => {
     res.redirect("/urls");
 });
-
 app.get("/urls", (req, res) => {
-    var templateVars = {urls : urlDatabase};
+    var templateVars = {
+        urls: urlDatabase
+    };
     res.render("urls_index", templateVars);
 });
-
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
+    let templateVars = {
+        shortURL: req.params.shortURL,
+        longURL: urlDatabase[req.params.shortURL]
+    };
+    res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);
+    res.send("Ok");
 });
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+
+}
