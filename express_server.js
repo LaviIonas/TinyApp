791f2b5dddc4545f5ac1540a6b2744ba425df446
LaviIonas <laviionas772@gmail.com>
2019-02-14 19:02:43 +0000

    Added another route for clicking on the tiny URL link

--- express_server.js
+++ express_server.js
@@ -1,26 +1,27 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+    res.redirect("/urls");
+});
+
+app.get("/urls", (req, res) => {
+    var templateVars = {urls : urlDatabase};
+    res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+    console.log(`Example app listening on port ${PORT}!`);
 });
