4b7bb51372b9ecbf206729e9da9c23a963fea0bc
LaviIonas <laviionas772@gmail.com>
2019-02-14 20:57:53 +0000

    added a url redirecter

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set("view engine", "ejs");
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,19 +27,27 @@ app.get("/urls/:shortURL", (req, res) => {
         shortURL: req.params.shortURL,
         longURL: urlDatabase[req.params.shortURL]
     };
+    //console.log(templateVars.longURL);
     res.render("urls_show", templateVars);
 });
-
 app.post("/urls", (req, res) => {
-    console.log(req.body);
-    res.send("Ok");
+    var shorturl = generateRandomString();
+    urlDatabase[shorturl] = req.body.longURL;
+    res.redirect(`/urls/${shorturl}`);
 });
-
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
 
-
 function generateRandomString() {
-
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 6; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
