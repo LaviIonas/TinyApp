f170fc9ba6fdb4e3365978f7a53e5a81f2d0a332
LaviIonas <laviionas772@gmail.com>
2019-02-14 21:19:26 +0000

    added delete function

--- express_server.js
+++ express_server.js
@@ -39,6 +39,10 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+app.post("/urls/:shortURL/delete", (req, res) => {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+});
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
