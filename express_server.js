ef1bf73dcf3b25df9d8d4944a9055c64cd8d7365
LaviIonas <laviionas772@gmail.com>
2019-03-12 13:49:02 +0000

    fixed the issue with the url object

--- express_server.js
+++ express_server.js
@@ -18,15 +18,12 @@ app.set("view engine", "ejs");
 var urlDatabase = {
     b2xVn2: {
         longURL: "http://www.lighthouselabs.ca",
-        userID: "userRandomID"
     },
     s9m5xK: {
         longURL: "http://www.google.com",
-        userID: "user2RandomID"
     },
     b3x4sc: {
         longURL: "http://woopyland.com",
-        userID: "user3RandomID"
     }
 };
 const users = {
@@ -56,7 +53,7 @@ function getName(ID) {
 }
 
 function generateRandomString(length) {
-    let text = "";
+    let text = '';
     const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (let i = 0; i < length; i++) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -110,6 +107,7 @@ app.get("/signup", (req, res) => {
 app.post("/signup", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+    if (password === '') throw ("Password is empty, please make a password (you must protect the links!!!)");
     const hashedPassword = bcrypt.hashSync(password, 10); //encrypts password
     checkIfExists(username, users); //should console.log ok if it passed
     const ID = generateRandomString(6); //generates random string
@@ -135,14 +133,16 @@ app.get("/urls", (req, res) => {
         username: name,
         ID: req.session.user_id,
         urls: urlDatabase
-    }
-    console.log(urlDatabase)
-    if(loggedIn){
-       res.render("urls_index", templateVars);
+    };
+    console.log("DB from index", urlDatabase);
+    console.log("|-----------------------------|");
+    if (loggedIn) {
+        res.render("urls_index", templateVars);
     } else {
         res.redirect("/login");
     }
 });
+// Get to create new link page
 app.get("/urls/new", (req, res) => {
     const name = getName(req.session.user_id);
     var templateVars = {
@@ -150,30 +150,39 @@ app.get("/urls/new", (req, res) => {
     }
     res.render("urls_new", templateVars);
 });
+// POST to accept new information and slap it into the DB
+app.post("/makeNewUrl", (req, res) => {
+    var ranID = generateRandomString(6);
+    urlDatabase[ranID] = {
+        longURL: req.body.longurl
+    };
+    res.redirect("/urls");
+});
+// Send the user to the long url link
+app.get("/u/:shortURL", (req, res) => {
+    //MUST be a http:// link or it will not recognize it
+    const sendLong = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(sendLong);
+});
+// -- forms from url-show ---
+// Get method upon clicking one of the short links which will take you to its individual page
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = {
         shortURL: req.params.shortURL,
-        longURL: urlDatabase[req.params.shortURL].longURL,
+        longURL: urlDatabase[req.params.shortURL].longURL
     };
     res.render("urls_show", templateVars);
 });
-app.post("/urls", (req, res) => {
-    var shorturl = generateRandomString(6);
-    urlDatabase[shorturl] = req.body.longURL;
-    res.redirect(`/urls/${shorturl}`);
-});
-app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL].longURL;
-    res.redirect(longURL);
+app.post("/urls/:shortURL/update", (req, res) => {
+    urlDatabase[req.params.shortURL].longURL = req.body.longurl;
+    res.redirect("/urls");
 });
 app.post("/urls/:shortURL/delete", (req, res) => {
     delete urlDatabase[req.params.shortURL];
     res.redirect("/urls");
 });
-app.post("/urls/:shortURL/update", (req, res) => {
-    urlDatabase[req.params.shortURL] = req.body.longURL;
-    res.redirect("/urls");
-});
+// -- port --
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
